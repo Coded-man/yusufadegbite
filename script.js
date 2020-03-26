f4010615f6b5cf9d7fd9d545b5d62447377cad3d
Coded-man <49336593+Coded-man@users.noreply.github.com>
2020-03-26 12:20:30 +0100

    Javascript task

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-<script type="text/javascript">
-    
-</script>
+var name = "Yusuf Adegbite";
+var courses = ["HTML", "CSS", "JavaScript", "Java", "Coding"];
+document.write(name + "<br>" + courses + "<br>");
+
+for (i=1;i<=200;i++) {
+if (i %2 !== 0) {
+    document.write(i + "<br>");
+}}
